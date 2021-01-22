b7471c3fe8ba96488e01597ba26ea8c572947c73
ayushmukho <52779201+ayushmukho@users.noreply.github.com>
2021-01-22 15:54:22 +0530

    Update firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyC-tlKuEkKX3DbWewiYMzQ0VoDe3gZPDhg",
-    authDomain: "slack-clone-39332.firebaseapp.com",
-    projectId: "slack-clone-39332",
-    storageBucket: "slack-clone-39332.appspot.com",
-    messagingSenderId: "75578507937",
-    appId: "1:75578507937:web:54113c76d53249a2a026e0",
-    measurementId: "G-CTX0ZXELF9"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
